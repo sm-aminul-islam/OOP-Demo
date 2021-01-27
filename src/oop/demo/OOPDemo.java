a5e2221e4674317ac8784f2a1484b31f9f09aaf6
devil <devil@DESKTOP-QBITRB3>
2021-01-27 20:02:38 +0600

    OOP

--- src/oop/demo/OOPDemo.java
+++ src/oop/demo/OOPDemo.java
@@ -6,14 +6,21 @@ public class OOPDemo {
 
    
     public static void main(String[] args) {
-   //Student s1 = new Student(113,"Robin");
-  // s1.getAddress();
+  //Student s1 = new Student();
+   //s1.getAddress();
   RationalNumber a = new RationalNumber(10,3);
   RationalNumber b = new RationalNumber(2,3);
   RationalNumber c = a.add(b);
   a.print();
   b.print();
   c.print();
+  ComplexNumber x = new ComplexNumber(2,4);
+  ComplexNumber y =new ComplexNumber(-2,4);
+  ComplexNumber z= x.Add(y);
+
+  x.print();
+  y.print();
+    z.print();
         
        
        // System.out.println("Id:");
